693f2e6ab7034852e17d1fe639e1f41c35a2f37f
Joffre <joffregh3002@gmail.com>
2024-11-07 17:22:38 -0500

    avance del heder de tesla

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,10 @@
 import animations from "@midudev/tailwind-animations";
-
+import typography from "@tailwindcss/typography;";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [animations],
+  plugins: [animations, require("@tailwindcss/typography")],
 };
